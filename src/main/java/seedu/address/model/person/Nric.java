62a8c9090ba153d811ea820e9f3f8ea28d87b1ca
Jun <leejunheng2001@gmail.com>
2025-03-12 17:39:27 +0800

    Add nric class

---
+++ src/main/java/seedu/address/model/person/Nric.java
@@ -0,0 +1,67 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's nric in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidNric(String)}
+ */
+public class Nric {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "NRIC should start with S or T, followed by xxx, 3 digit number and ends with an alphabet" +
+                    " and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "^[ST]xxxx\\d{3}[A-Z]$";
+
+    public final String maskNric;
+
+    /**
+     * Constructs a {@code Nric}.
+     *
+     * @param nric A valid masked nric.
+     */
+    public Nric(String nric) {
+        requireNonNull(nric);
+        checkArgument(isValidNric(nric), MESSAGE_CONSTRAINTS);
+        maskNric = nric;
+    }
+
+    /**
+     * Returns true if a given string is a valid masked nric.
+     */
+    public static boolean isValidNric(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return maskNric;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Nric)) {
+            return false;
+        }
+
+        Nric otherNric = (Nric) other;
+        return maskNric.equals(otherNric.maskNric);
+    }
+
+    @Override
+    public int hashCode() {
+        return maskNric.hashCode();
+    }
+
+}
