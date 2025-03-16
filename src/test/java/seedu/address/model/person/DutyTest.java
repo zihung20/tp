f64ce27b5a02292bbce9aa05707de89afd509a57
zihung20 <zihung20@gmail.com>
2025-03-16 14:56:24 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/model/person/DutyTest.java
+++ src/test/java/seedu/address/model/person/DutyTest.java
@@ -8,7 +8,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
