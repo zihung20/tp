ea0d0bbf74bc4b215767e23fd2c79e7a47f62166
zihung20 <zihung20@gmail.com>
2025-03-20 21:02:45 +0800

    Fix merge conflict

--- src/test/java/seedu/address/logic/parser/AssignCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AssignCommandParserTest.java
@@ -4,9 +4,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.DUTY_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DUTY_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_DUTY_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DUTY_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DUTY_AMY_STRING;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DUTY_BOB_STRING;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -73,25 +71,13 @@ public class AssignCommandParserTest {
 
     @Test
     public void parse_multiple_duty() {
-<<<<<<< HEAD
         String userInput = INDEX_FIRST_PERSON.getOneBased() + DUTY_DESC_AMY + DUTY_DESC_BOB;
-=======
-        Index targetIndex = INDEX_FIRST_PERSON;
-        String userInput = targetIndex.getOneBased() + DUTY_DESC_AMY + DUTY_DESC_BOB;
-        AssignCommand expectedCommand = new AssignCommand(targetIndex, VALID_DUTY_BOB_STRING);
->>>>>>> master
 
         //only record the last appearance of duty date
         assertParseFailure(parser, userInput,
                 Messages.getErrorMessageForDuplicatePrefixes(CliSyntax.PREFIX_DUTY));
 
-<<<<<<< HEAD
         String secondUserInput = INDEX_SECOND_PERSON.getOneBased() + DUTY_DESC_BOB + DUTY_DESC_AMY;
-=======
-        Index secondTargetIndex = INDEX_SECOND_PERSON;
-        String secondUserInput = secondTargetIndex.getOneBased() + DUTY_DESC_BOB + DUTY_DESC_AMY;
-        AssignCommand secondExpectedCommand = new AssignCommand(secondTargetIndex, VALID_DUTY_AMY_STRING);
->>>>>>> master
 
         //only record the last appearance of duty date
         assertParseFailure(parser, secondUserInput,
