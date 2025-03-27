85a3fa4dcd102381cdbc46623dde9df8c3faaeab
Lee Chen Xuan <e1129901@u.nus.edu>
2025-03-27 21:46:33 +0800

    Add rules to user's input for AssignCommand

--- src/main/java/seedu/address/logic/commands/ReassignCommand.java
+++ src/main/java/seedu/address/logic/commands/ReassignCommand.java
@@ -1,9 +1,10 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -19,13 +20,16 @@ public class ReassignCommand extends Command {
     public static final String MESSAGE_USAGE = String.format(
         "%s: Reassign a duty date to personnel by specifying the identity of the personnel. "
                     + "Duplicate dates will be considered the same.\n"
+                    + "Old duty date shouldn't be the same as new duty date.\n"
                     + "Parameters: INDEX, %sDUTY_DATE_TO_BE_CHANGED, %sNEW_DUTY_DATE\n"
                     + "Example: reassign 1 d/2025-04-15 nd/2025-04-26",
             COMMAND_WORD, CliSyntax.PREFIX_DUTY, CliSyntax.PREFIX_NEW_DUTY);
     public static final String MESSAGE_REASSIGN_DUTY_SUCCESS = 
         "Success! Duty reassigned to person %1$s!";
-    public static final String MESSAGE_REASSIGN_DUTY_FAILED =
+    public static final String MESSAGE_REASSIGN_DUTY_FAILED_DUTY_NOT_FOUND =
         "Failed! The personnel's old duty cannot be found! %1$s";
+    public static final String MESSAGE_REASSIGN_DUTY_FAILED_SAME_DATE =
+        "Failed! The old duty date shouldn't be the same as the new duty date! %1$s";
 
     private final Index index;
     private final String oldDutyDate;
@@ -53,12 +57,16 @@ public class ReassignCommand extends Command {
         }
 
         Person personToReassign = lastShownList.get(index.getZeroBased());
+
+        if (oldDutyDate.equals(newDutyDate)) {
+            throw new CommandException(String.format(MESSAGE_REASSIGN_DUTY_FAILED_SAME_DATE, Messages.format(personToReassign)));
+        }
+
         if (personToReassign.unassignDuty(oldDutyDate)) {
             personToReassign.assignDuty(newDutyDate);
-            model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
             return new CommandResult(String.format(MESSAGE_REASSIGN_DUTY_SUCCESS, Messages.format(personToReassign)));
         } else {
-            throw new CommandException(String.format(MESSAGE_REASSIGN_DUTY_FAILED,
+            throw new CommandException(String.format(MESSAGE_REASSIGN_DUTY_FAILED_DUTY_NOT_FOUND,
             Messages.format(personToReassign)));
         }
     }
