5ae8ad3b0618383133950fbfd723fc31f6883c07
CoreySiah <coreysiah@gmail.com>
2025-03-27 22:31:48 +0800

    Add filter command and related test cases

---
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.CompanyContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
+
+/**
+ * Parses input arguments and creates a new FilterCommand object
+ */
+public class FilterCommandParser implements Parser<FilterCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FilterCommand
+     * and returns a FilterCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FilterCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_COMPANY);
+
+        if (!argMultimap.getValue(PREFIX_COMPANY).isPresent()
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+        }
+
+        String companyArgs = argMultimap.getValue(PREFIX_COMPANY).get();
+        String[] companyKeywords = companyArgs.trim().split("\\s+");
+
+        return new FilterCommand(new CompanyContainsKeywordsPredicate(Arrays.asList(companyKeywords)));
+    }
+}
